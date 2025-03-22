d71295865e98977c69b8d9061c4eca520bab016b
Jamal Bendadi <70474057+jamalbendadi@users.noreply.github.com>
2025-03-22 15:28:18 +0000

    analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { Analytics } from '@vercel/analytics/nuxt'
 </script>
 <template>
   <SpeedInsights/>
+  <Analytics/>
   <div class="bg-black">
     <!--<MainHeader title="Jamal Bendadi, Software Engineer and AI enthusiast" />-->
     <slot />
